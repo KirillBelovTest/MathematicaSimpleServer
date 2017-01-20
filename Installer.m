147b0d4356324a3f37c2c8c6a84953a277126efa
Kirill Belov <KirillBelovTest@gmail.com>
2017-01-20 18:27:33 +0400

    added installer

---
+++ Installer.m
@@ -0,0 +1,54 @@
+(* ::Package:: *)
+
+(* :Title: Installer *)
+
+(* :Author: Kirill Belov *)
+(* :Email: KirillBelovTest@gmail.com *)
+
+Begin["Installer`Private`"]; 
+
+Block[
+{
+	$ProjectDirectory, 
+	$TargetKernelDirectory, 
+	$ApplicationInitFilePath, 
+	$ApplicationContext, 
+	Extensions, 
+	Name
+}, 
+	
+$ProjectDirectory = DirectoryName[$InputFileName]; 
+	
+SetDirectory[$ProjectDirectory]; 
+	
+$TargetKernelDirectory = 
+FileNameJoin[
+	{
+		$UserBaseDirectory, "Applications", 
+		(Association @@ Get["PacletInfo.m"])[[Key[Name]]], 
+		"Kernel"
+	}
+]; 
+	
+$ApplicationInitFilePath = FileNameJoin[{$TargetKernelDirectory, "Init.m"}]; 
+	
+$ApplicationContext = 
+Association[Rest[
+	(Association @@ Get["PacletInfo.m"])[[Key[Extensions]]][[1]]
+]][[Key[Context]]]; 
+
+If[!FileExistsQ[$TargetKernelDirectory], CreateDirectory[$TargetKernelDirectory]]; 
+
+With[{ProjectDirectory = $ProjectDirectory, ApplicationContext = $ApplicationContext}, 
+	With[{code = Unevaluated[$Path = DeleteDuplicates[Prepend[$Path, ProjectDirectory]]; Get[ApplicationContext, Path -> {ProjectDirectory}];]}, 
+		Put[code, $ApplicationInitFilePath]; 
+	]; 
+]; 
+	
+ResetDirectory[]; 
+
+Remove["Installer`Private`*"]; 
+
+]; 
+
+End[]; (*Installer`Private`*)
