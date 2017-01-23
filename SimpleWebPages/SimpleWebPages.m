740c708b201808aaa68488b501377a4fbc404c17
Kirill Belov <KirillBelovTest@gmail.com>
2017-01-23 18:47:28 +0400

    Update SimpleWebPages.m

--- SimpleWebPages/SimpleWebPages.m
+++ SimpleWebPages/SimpleWebPages.m
@@ -3,9 +3,6 @@
 BeginPackage["MathematicaSimpleServer`WebPages`"];
 (* Exported symbols added here with SymbolName::usage *)  
 
-SimpleWebServerCreate::usage = 
-"SimpleWebServerCreate[port]"; 
-
 Begin["`Private`"]; (* Begin Private Context *) 
 
 End[]; (* End Private Context *)
