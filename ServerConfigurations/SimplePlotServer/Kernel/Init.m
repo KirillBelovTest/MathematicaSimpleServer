30702648cdb09a41f02236e63b321d31966e29da
Kirill Belov <KirillBelovTest@gmail.com>
2017-01-24 16:25:33 +0400

    update

---
+++ ServerConfigurations/SimplePlotServer/Kernel/Init.m
@@ -0,0 +1,5 @@
+(* ::Package:: *)
+
+(* Wolfram Language package *)
+
+Get["SimplePlotServer`SimplePlotServer`"];
---
+++ ServerConfigurations/SimplePlotServer/SimplePlotServer.m
@@ -0,0 +1,18 @@
+(* ::Package:: *)
+
+(* :Title: SimplePlotServer *)
+(* :Context: SimplePlotServer` *)
+
+(* :Author: Kirill Belov *)
+(* :Email: KirillBelovTest@gmail.com *)
+
+BeginPackage["MathematicaSimpleServer`SimplePlotServer`", {"MathematicaSimpleServer`"}]; 
+
+SimplePlotServerCreate::usage = 
+"SimplePlotServerCreate[port]"; 
+
+Begin["`Private`"]; 
+
+End[] (*`Private`*)
+
+EndPackage[]; (* MathematicaSimpleServer`SimplePlotServer` *)
