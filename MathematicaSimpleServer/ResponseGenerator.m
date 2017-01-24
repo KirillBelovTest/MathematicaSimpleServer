a76120dea84d25b8b2bb01328b049f23744abe6f
Kirill Belov <KirillBelovTest@gmail.com>
2017-01-24 22:38:51 +0400

    update

--- MathematicaSimpleServer/ResponseGenerator.m
+++ MathematicaSimpleServer/ResponseGenerator.m
@@ -8,8 +8,59 @@ ResponseGenerator::usage =
 
 Begin["`Private`"]; (* Begin Private Context *) 
 
+(* internal symbol *)
+Component::usage = 
+"ResponseGenerator[{key1 -> component1, key2 -> ...}][[Component[key1]]] >> component1"; 
 
+(* internal name *)
+Component::usage = "Internal symbol. Using: ServerBean[[Component[key]]]"; 
+
+ResponseGenerator::srverr = 
+"Inernal server error"; 
+
+(* 
+	override Keys on the request parser type 
+	Keys[ResponseGenerator[{key1 -> component1, key2 -> ...}]] >> {key1, key2, ...}
+*)
+ResponseGenerator /: 
+Keys[responseGenerator_ResponseGenerator] /; 
+MatchQ[
+	responseGenerator, 
+	ResponseGenerator[
+		{
+			Rule[
+				_String, 
+				(_Symbol | _Function) 
+			] ..
+		}
+	]
+
+] := responseGenerator[[1, All, 1]]; 
+
+(* override [[..]] on the response generator *)
+ResponseGenerator /: 
+Part[
+	ResponseGenerator[
+		{
+			___Rule, 
+			key_String -> component: 
+			(
+				_Symbol | 
+				_Function
+			), 
+			___Rule
+		}
+	], 
+	Component[key_String]
+] := component; 
+
+(* main definition *)
+responseGenerator_ResponseGenerator[key_String -> content_] /; 
+MemberQ[Keys[responseGenerator], key] := 
+responseGenerator[[Component[key]]][content]; 
+
+responseGenerator_ResponseGenerator[$Failed] := (Message[ResponseGenerator::srverr]; $Failed); 
 
 End[]; (* End Private Context *)
 
-EndPackage[];
+EndPackage[]; 
