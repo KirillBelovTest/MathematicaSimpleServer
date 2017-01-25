c0c67b334a20af93c0c364602f579fffc7cc933d
Kirill Belov <KirillBelovTest@gmail.com>
2017-01-25 08:08:06 +0400

    update

--- ExampleServerConfiguration/PlotServer/PlotServer.m
+++ ExampleServerConfiguration/PlotServer/PlotServer.m
@@ -7,4 +7,36 @@
 (* :Email: KirillBelovTest@gmail.com *)
 
 PlotServer::usage = 
-"PlotServer[port]"; 
+"PlotServer[port] >> MathematicaSimpleServer[...]"; 
+
+(* function for the creation of the request parser *)
+PlotRequestQ[request_String] := 
+StringContainsQ[
+	ToLowerCase[First[StringSplit[request, "\r\n"]]], 
+	"get /plotserver?" ~~ ___ ~~ "http/1.1"
+];
+
+PlotRequestToIndexQ[request_String] := 
+ToLowerCase[First[StringSplit[request, "\r\n"]]] == "get / http/1.1" || 
+ToLowerCase[First[StringSplit[request, "\r\n"]]] == "get /index http/1.1" || 
+ToLowerCase[First[StringSplit[request, "\r\n"]]] == "get /plotserver http/1.1"; 
+
+PlotRequestAddressTake[request_String] := 
+First[StringCases[
+	First[StringSplit[request, "\r\n"]], 
+	"GET " ~~ address__ ~~ " HTTP/1.1" :> address
+]]; 
+
+(* function for the creation of the request parser *)
+PlotBean[address_String] := 
+
+PlotIndexPageBean[_String] := 
+ReadString[
+	FileNameJoin[
+		{
+			DirectoryName[$InputFileName], 
+			"Resources", 
+			"Index.html"
+		}
+	]
+];
